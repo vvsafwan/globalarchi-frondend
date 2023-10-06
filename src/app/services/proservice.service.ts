2210311de9d181b1bafeba01bb9b1530379ef2e0
vvsafwan <vvsafwan2002@gmail.com>
2023-10-06 10:30:02 +0530

    deployupdations

--- src/app/services/proservice.service.ts
+++ src/app/services/proservice.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 export class ProserviceService {
 
-  apiUrl: string = "http://localhost:3000/pro"
+  apiUrl: string = "https://globalarchi.online/pro"
 
   constructor(
     private http: HttpClient
